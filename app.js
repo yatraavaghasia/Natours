4cf9f11d1814c68966a53f53427b9caee3d7e9cf
yatraavaghasia <113079616+yatraavaghasia@users.noreply.github.com>
2023-08-27 19:13:53 +0530

    Update app.js. Please god, let this be the final time.🚀

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const morgan = require('morgan');
 const path = require('path');
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-const csp = require('express-csp');
-// const compression = require('compression');
+// const csp = require('express-csp');
+const compression = require('compression');
 const mongoSanitize = require('express-mongo-sanitize');
 
 const xss = require('xss-clean');
@@ -31,81 +31,121 @@ const bookingRouter = require('./routes/bookingRoutes');
 //Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// console.log(process.env.NODE_ENV);
-//set security http headers
-app.use(helmet());
-csp.extend(app, {
-  policy: {
-    directives: {
-      'default-src': ['self'],
-      'style-src': ['self', 'unsafe-inline', 'https:'],
-      'font-src': ['self', 'https://fonts.gstatic.com'],
-      'script-src': [
-        'self',
-        'unsafe-inline',
-        'data',
-        'blob',
-        'https://js.stripe.com',
-        'https://*.mapbox.com',
-        'https://*.cloudflare.com/',
-        'https://bundle.js:8828',
-        'ws://localhost:56558/',
-      ],
-      'worker-src': [
-        'self',
-        'unsafe-inline',
-        'data:',
-        'blob:',
-        'https://*.stripe.com',
-        'https://*.mapbox.com',
-        'https://*.cloudflare.com/',
-        'https://bundle.js:*',
-        'ws://localhost:*/',
-      ],
-      'frame-src': [
-        'self',
-        'unsafe-inline',
-        'data:',
-        'blob:',
-        'https://*.stripe.com',
-        'https://*.mapbox.com',
-        'https://*.cloudflare.com/',
-        'https://bundle.js:*',
-        'ws://localhost:*/',
-      ],
-      'img-src': [
-        'self',
-        'unsafe-inline',
-        'data:',
-        'blob:',
-        'https://*.stripe.com',
-        'https://*.mapbox.com',
-        'https://*.cloudflare.com/',
-        'https://bundle.js:*',
-        'ws://localhost:*/',
-      ],
-      'connect-src': [
-        'self',
-        'unsafe-inline',
-        'data:',
-        'blob:',
-        // 'wss://natours-yatra4.onrender.com:56341/',
-        'https://*.stripe.com',
-        'https://*.mapbox.com',
-        'https://*.cloudflare.com/',
-        'https://bundle.js:*',
-        'ws://localhost:*/',
-      ],
-    },
-  },
-});
+const scriptSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://js.stripe.com',
+  ' https://cdnjs.cloudflare.com',
+];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/',
+  // 'https://js.stripe.com'
+];
+const connectSrcUrls = [
+  'https://unpkg.com',
+  'https://tile.openstreetmap.org',
+  'https://js.stripe.com',
+  'https://cdnjs.cloudflare.com',
+  'ws://127.0.0.1:*/',
+];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
 
 app.use(
-  cors({
-    credentials: true,
-    origin: 'http://localhost:3000'
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'data:', 'blob:', 'https:', 'ws:'],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      frameSrc: ["'self'", 'https://js.stripe.com'],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      fontSrc: ["'self'", ...fontSrcUrls],
+      upgradeInsecureRequests: [],
+    },
   })
 );
+
+
+
+// console.log(process.env.NODE_ENV);
+//set security http headers
+// app.use(helmet());
+// csp.extend(app, {
+//   policy: {
+//     directives: {
+//       'default-src': ['self'],
+//       'style-src': ['self', 'unsafe-inline', 'https:'],
+//       'font-src': ['self', 'https://fonts.gstatic.com'],
+//       'script-src': [
+//         'self',
+//         'unsafe-inline',
+//         'data',
+//         'blob',
+//         'https://js.stripe.com',
+//         'https://*.mapbox.com',
+//         'https://*.cloudflare.com/',
+//         'https://bundle.js:8828',
+//         'ws://localhost:56558/',
+//       ],
+//       'worker-src': [
+//         'self',
+//         'unsafe-inline',
+//         'data:',
+//         'blob:',
+//         'https://*.stripe.com',
+//         'https://*.mapbox.com',
+//         'https://*.cloudflare.com/',
+//         'https://bundle.js:*',
+//         'ws://localhost:*/',
+//       ],
+//       'frame-src': [
+//         'self',
+//         'unsafe-inline',
+//         'data:',
+//         'blob:',
+//         'https://*.stripe.com',
+//         'https://*.mapbox.com',
+//         'https://*.cloudflare.com/',
+//         'https://bundle.js:*',
+//         'ws://localhost:*/',
+//       ],
+//       'img-src': [
+//         'self',
+//         'unsafe-inline',
+//         'data:',
+//         'blob:',
+//         'https://*.stripe.com',
+//         'https://*.mapbox.com',
+//         'https://*.cloudflare.com/',
+//         'https://bundle.js:*',
+//         'ws://localhost:*/',
+//       ],
+//       'connect-src': [
+//         'self',
+//         'unsafe-inline',
+//         'data:',
+//         'blob:',
+//         // 'wss://natours-yatra4.onrender.com:56341/',
+//         'https://*.stripe.com',
+//         'https://*.mapbox.com',
+//         'https://*.cloudflare.com/',
+//         'https://bundle.js:*',
+//         'ws://localhost:*/',
+//       ],
+//     },
+//   },
+// });
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: 'http://localhost:3000'
+//   })
+// );
 
 //development logging
 if (process.env.NODE_ENV === 'development') {
@@ -142,7 +182,7 @@ app.use(
   })
 );
 
-// app.use(compression());
+app.use(compression());
 
 //Test Middleware
 app.use((req, res, next) => {
