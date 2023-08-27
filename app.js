c884ef934166e387aaf3421a01a00b400f78db22
yatraavaghasia <113079616+yatraavaghasia@users.noreply.github.com>
2023-08-27 18:34:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const csp = require('express-csp');
-const compression = require('compression');
+// const compression = require('compression');
 const mongoSanitize = require('express-mongo-sanitize');
 
 const xss = require('xss-clean');
@@ -142,7 +142,7 @@ app.use(
   })
 );
 
-app.use(compression());
+// app.use(compression());
 
 //Test Middleware
 app.use((req, res, next) => {
